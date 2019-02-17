aa38496a3624c1847a50dc3a50b65fc3aa924831
autochilli <si@geeky-teacher.co.uk>
2019-02-17 15:07:25 +0800

    update iss

--- 101computing_challenges/iss.py
+++ 101computing_challenges/iss.py
@@ -1,7 +1,10 @@
 import requests
 
-# Make a get request to get the latest position of the international space station from the opennotify api.
-response = requests.get("http://api.open-notify.org/iss-now.json")
+parameters = {"lat": 37.78, "lon": -122.41}
 
-# Print the status code of the response.
-print(response.status_code)
+# Make a get request to get the latest position of the international space station from the opennotify api.
+response = requests.get("http://api.open-notify.org/iss-pass.json", params=parameters)
+
+data = response.json()
+print(type(data))
+print(data)
