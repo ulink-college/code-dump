763cfb2ae266a8513a19ce75e6127fccfd8321be
autochilli <si@geeky-teacher.co.uk>
2019-02-21 14:15:44 +0800

    moved student 'sent' work in to new folder

---
+++ StudentWork/Nicolas-09B/1.py
@@ -0,0 +1,16 @@
+riddle = "what has 4 eyes but cannot see?"
+answer = "mississippi"
+userAnswer = ""
+attempt = 0
+userAnswer = input(riddle)
+while  userAnswer != answer:
+    userAnswer = input(riddle)
+    attempt += 1
+    if userAnswer == answer:
+        print("well done")
+        while attempt <= 3
+                print("well done!!!!!")
+            
+    else:
+        print("sorry,thats not the answer")
+        print("You have had",attempt,"attempts")
