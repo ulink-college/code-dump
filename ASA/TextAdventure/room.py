8431b8412f027506cec936ab88ba0d3dee49179f
CSSFA <si@autochilli.com>
2019-05-01 14:57:43 +0800

    room class - step 1

---
+++ ASA/TextAdventure/room.py
@@ -0,0 +1,6 @@
+#Room Class
+
+class Room():                       # Start classes with a capital letter to differentiate from variables
+    def __init__(self):             # method used to create (initialise) object from this class
+        self.name = None            # attributes for the object
+        self.description = None
